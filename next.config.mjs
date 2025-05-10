8410bfae20c45a31d2286c79a816af8c408f1a56
williamk19 <williamkurniawan1144@gmail.com>
2025-05-10 19:42:20 +0700

    add images remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'pb.williamk19.com',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-      },
-    ],
-  },
-  experimental: {
-    webpackMemoryOptimizations: true,
-  },
-  webpack: (config) => {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    };
-    return config;
-  },
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "pb.williamk19.com",
+            },
+            {
+                protocol: "https",
+                hostname: "williamk19-pb.fly.dev",
+            },
+            {
+                protocol: "http",
+                hostname: "localhost",
+            },
+        ],
+    },
+    experimental: {
+        webpackMemoryOptimizations: true,
+    },
+    webpack: (config) => {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        };
+        return config;
+    },
 };
 
 export default nextConfig;
