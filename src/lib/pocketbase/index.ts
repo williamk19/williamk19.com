78161b2fb6d18713edbde66b5a611809faa27b02
williamk19 <williamkurniawan1144@gmail.com>
2025-12-19 16:40:41 +0700

    fixing in pocketbase connection

--- src/lib/pocketbase/index.ts
+++ src/lib/pocketbase/index.ts
@@ -4,6 +4,9 @@ const username = process.env.NEXT_PUBLIC_PB_USERNAME;
 const password = process.env.NEXT_PUBLIC_PB_PASSWORD;
 
 const pb = new PocketBase(process.env.NEXT_PUBLIC_PB_URL);
+
+pb.autoCancellation(false);
+
 const authData = await pb
     .collection("users")
     .authWithPassword(username!, password!);
